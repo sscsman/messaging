fd49c5a2d67072bcbbf24e575012cf5e60783e20
sscsman <sscsman@gmail.com>
2017-04-18 13:26:19 +0900

    l

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/boards', boards);
-app.use('/register', register); 
+
+app.use('/register', register);
 
 app.use('/', index);
 app.use('/users', users);
