83de5b810f3c698f7f166ea95ef02c23c1817888
sscsman <sscsman@gmail.com>
2017-04-20 04:33:56 +0900

    soc

---
+++ routes/socketio.js
@@ -0,0 +1,34 @@
+//appモジュールを読み込む（このappには、www内ですでにポート番号が登録されているので、ポート番号を取得したことにもなる）
+var app = require('../app');
+
+//httpモジュールを読み込んで、サーバーを立ち上げる（createServerと同じ扱い）
+var http = require('http');
+
+//必要な三要素を揃えて初めてサーバーを立ち上げられる
+var server = http.createServer(app);
+
+//サーバーをlistenしてsocketIOを設定
+var io = require('socket.io')(server);
+
+//socketIOモジュール
+function socketio(){
+    //サーバーを立ち上げたら実行
+    server.listen(app.get('port'), function() {
+        console.log('listening!!!');
+    });
+
+    //socket処理を記載する
+    io.on('connection', function(socket){
+                  console.log('IO!!');
+        //socket処理
+        socket.on('socketName',function(data){
+
+            console.log(data);
+            io.sockets.emit('socketName2',data);
+        })
+
+    });
+};
+
+//export
+module.exports = socketio;
